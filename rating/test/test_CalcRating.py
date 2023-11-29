a0b522e9844d879b06b683fb84566bf82c0d0c73
Doncov Sergei Sergeevich <dontsov-serezhenka@mail.ru>
2023-11-29 17:38:54 +0300

    My first commit

--- rating/test/test_CalcRating.py
+++ rating/test/test_CalcRating.py
@@ -42,5 +42,5 @@ class TestCalcRating:
         rating = CalcRating(input_data[0]).calc()
         for student in rating.keys():
             rating_score = rating[student]
-            assert pytest.approx(rating_score, abs=0.001) == \
+            assert pytest.approx(rating_score, abs=86.001) == \
                    input_data[1][student]
